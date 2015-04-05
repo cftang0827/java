23334044334e8e0ec4b25db7dedcb50be2df8839
cftang0827 <cftang0827@gmail.com>
2015-04-06 04:02:51 +0800

    there are .class file and something modification about .java file it is down.(hw3)

--- hw3/Main.java
+++ hw3/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
 		MyClass myClass = new MyClass();
-		String inputText  = "Hello Hello World";		
+		String inputText  = "Hello Hello World, I love love the World";		
+		
 		String replacedText = myClass.removeDuplicateWords(inputText);
 		System.out.println(replacedText);
 	}
--- hw3/MyClass.java
+++ hw3/MyClass.java
@@ -1,12 +1,27 @@
+/*
+FILE NAME: MyClass.java
+AUTHUR NAME:CHIH FANG TAN
+SCHOOL ID:B00404017
+LAST MODIFY DATE:2015/4/6
+THIS IS HOMEWORK 3 OF OBJECT ORIENT PROGRAMMING COURSE
+*/
+
 public class MyClass {
 	public String removeDuplicateWords(String inputText){
-		/*
-  		 * your code here
-  		 * 
-  		 */
+		String output = new String();//declare new string to store output
+		String[] sub_string = inputText.split(" ");//split original string to small piece
 		
-		return null;
+		output = sub_string[0]+" ";//initialize output with first element
+		for(int j=0;j<sub_string.length;j++)
+		{
+		 	if(! output.contains(sub_string[j]))
+			{
+				output = output + sub_string[j]+" ";
+			}
+		}
+		//find whether there are same substring in the output, if so do not add in the output string
+		return output;
 	}
 	
-	//TODO: any other methods are optional here.
+	
 }
